0e2a2db635d71786d5cafe7fd5c78a280aa8383b
NigarAliyeva1 <112957859+NigarAliyeva1@users.noreply.github.com>
2023-06-13 16:27:54 +0400

    Create aggregate_functions.sql

---
+++ task1/aggregate_functions.sql
@@ -0,0 +1,46 @@
+SELECT
+    *
+FROM
+    employees;
+
+
+
+SELECT
+    SUM(salary)
+FROM
+    employees;
+
+
+
+SELECT
+    COUNT(salary)
+FROM
+    employees;
+
+
+
+SELECT
+    MIN(salary)
+FROM
+    employees;
+
+
+
+SELECT
+    MAX(salary)
+FROM
+    employees;
+
+
+
+SELECT
+    AVG(salary)
+FROM
+    employees;
+
+
+
+SELECT
+    MEDIAN(salary)
+FROM
+    employees;
