f26b8f17c80c6230b5cfd7cad24508e414c6d389
NigarAliyeva1 <112957859+NigarAliyeva1@users.noreply.github.com>
2023-06-16 16:56:37 +0400

    Update joins.sql

--- task1/joins.sql
+++ task1/joins.sql
@@ -25,3 +25,45 @@ SELECT
 FROM
          employees
     JOIN departments USING ( department_id );
+
+
+     
+SELECT
+    e.employee_id
+FROM
+    employees   e
+    LEFT JOIN departments d ON e.employee_id = d.department_id;
+
+SELECT
+    e.employee_id
+FROM
+    employees   e
+    RIGHT JOIN departments d ON e.employee_id = d.department_id;
+
+SELECT
+    e.employee_id
+FROM
+    employees   e
+    FULL JOIN departments d ON e.employee_id = d.department_id;
+
+SELECT
+    *
+FROM
+         employees e
+    CROSS JOIN departments d
+WHERE
+    e.employee_id = d.department_id;
+
+SELECT
+    a.department_id,
+    a.department_name,
+    e.employee_id
+FROM
+    departments a,
+    employees   e
+WHERE
+    e.employee_id BETWEEN 100 AND 250
+    AND a.department_id < 40;
+select * from employees;
+AND a.department_id < 40; 
+    
