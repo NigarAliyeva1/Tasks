4699c622f93883f5c324097066379330194b3268
NigarAliyeva1 <112957859+NigarAliyeva1@users.noreply.github.com>
2023-06-16 16:48:12 +0400

    Update set_operators.sql

--- task1/set_operators.sql
+++ task1/set_operators.sql
@@ -50,3 +50,31 @@ SELECT
     manager_id
 FROM
     employees;
+
+
+
+SELECT
+    first_name
+FROM
+    employees
+UNION
+SELECT
+    email
+FROM
+    employees
+INTERSECT
+SELECT
+    department_name
+FROM
+    departments
+MINUS
+SELECT
+    job_title
+FROM
+    jobs
+UNION ALL
+SELECT
+    last_name
+FROM
+    employees;
+
