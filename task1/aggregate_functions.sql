dc0d2cbe7f94bbf6501aca4b481a19a44712a6e5
NigarAliyeva1 <112957859+NigarAliyeva1@users.noreply.github.com>
2023-06-16 16:19:06 +0400

    Update aggregate_functions.sql

--- task1/aggregate_functions.sql
+++ task1/aggregate_functions.sql
@@ -1,44 +1,79 @@
-SELECT
-    *
-FROM
-    employees;
-
-
-
+    
 SELECT
     SUM(salary)
 FROM
     employees;
 
-
+SELECT
+    first_name,
+    SUM(salary)
+FROM
+    employees
+GROUP BY
+    first_name;
 
 SELECT
     COUNT(salary)
 FROM
     employees;
 
-
+SELECT
+    first_name,
+    COUNT(salary)
+FROM
+    employees
+GROUP BY
+    first_name;
 
 SELECT
+    first_name,
     MIN(salary)
 FROM
-    employees;
+    employees
+GROUP BY
+    first_name;
 
+SELECT
+    first_name,
+    MIN(salary)
+FROM
+    employees
+GROUP BY
+    first_name;
 
+SELECT
+    first_name,
+    MAX(salary)
+FROM
+    employees
+GROUP BY
+    first_name;
+
+SELECT
+    first_name,
+    AVG(salary)
+FROM
+    employees
+GROUP BY
+    first_name;
+
+SELECT
+    first_name,
+    MEDIAN(salary)
+FROM
+    employees
+GROUP BY
+    first_name;
 
 SELECT
     MAX(salary)
 FROM
     employees;
 
-
-
 SELECT
     AVG(salary)
 FROM
     employees;
-
-
 
 SELECT
     MEDIAN(salary)
